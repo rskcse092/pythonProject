a91e2502f2c8f598d80e35df0566e1e536e66f27
Ramshankar Kumar <54095910+rskcse092@users.noreply.github.com>
2023-06-25 05:39:01 +0530

    Added the code of inheritance and polymorphism in oops.py

--- MASTER/oops.py
+++ MASTER/oops.py
@@ -105,4 +105,116 @@ ob.dispC()
 
 # by using child class object we can access both two parent class properties
 
+#hierarachical
+
+class A():
+    a = 5
+
+    def dispA(self):
+        print("this is A")
+        print(f"value of a IS are:{self.a}")
+
+class B(A):
+    b = 10
+
+    def dispB(self):
+        print("this is B")
+        print(f"value of B is {self.b}")
+class C(A):
+    def dispC(self):
+        print("this is C")
+
+obc = C()
+obc.dispA()
+
+obb = B()
+obb.dispA()
+
+
+#hybrid
+
+class A():
+    a = 5
+
+    def dispA(self):
+        print("this is A")
+        print(f"value of a IS are:{self.a}")
+
+class B(A):
+    b = 10
+
+    def dispB(self):
+        print("this is B")
+        print(f"value of B is {self.b}")
+class C(A):
+    def dispC(self):
+        print("this is C")
+
+class D():
+    def dispD(self):
+        print("this is D")
+
+class E(A,D):
+    def dispE(self):
+        print("this is E")
+
+obe = E()
+obe.dispA()
+obe.dispD()
+
+obb = B()
+obb.dispA()
+
+obc = C()
+obb.dispA()
+
 #polymorphism
+
+#buildin function in python which shows polymorphism
+l = [2,3,4,5]
+s = "RAM"
+print(len(l))
+print(len(s))
+
+
+#method overiding
+class A():
+    def __init__(self):
+        print("init constructor of parent class")
+    def disp(self):
+        print("this is A")
+
+
+class B(A):
+    def __init__(self):
+        print("init constructor of child class")
+    def disp(self):
+        print("this is B")
+
+ob = B()
+ob.disp()
+
+#method overloding
+def add(x, y, z=0):
+        print( x + y + z)
+
+
+add(1,2)
+add(1,2,3)
+
+
+#constructor overiding
+
+class A:
+    def __init__(self):
+        print("parent class init constructor")
+
+class B(A):
+    def __init__(self):
+        print("child class init constructor")
+
+obb = B()
+obb.__init__()
+
+
+#constructor overloading is not suppoted in python as it consider last constructor if we give multiple constructor
